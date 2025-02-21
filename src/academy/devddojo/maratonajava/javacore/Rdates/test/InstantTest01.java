80205c32e76773b49c5a1a75715c1ef527fe91e6
Alisson <alissonvictorxd@gmail.com>
2025-02-21 13:05:39 -0300

    instant addicionado

--- src/academy/devddojo/maratonajava/javacore/Rdates/test/InstantTest01.java
+++ src/academy/devddojo/maratonajava/javacore/Rdates/test/InstantTest01.java
@@ -9,7 +9,7 @@ public class InstantTest01 {
         Instant now = Instant.now();//Trabalha com nano segundos
         System.out.println(now);
         System.out.println(LocalDateTime.now());
-        System.out.println(now.getEpochSecond() b);
+        System.out.println(now.getEpochSecond() );
         System.out.println(now.getNano());
 
 
