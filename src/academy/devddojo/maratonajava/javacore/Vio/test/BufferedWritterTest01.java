b547476c37b3d25c13bfdd6822c8d72a946b6aa3
Alisson <alissonvictorxd@gmail.com>
2025-03-01 20:15:18 -0300

    Aula 141 BufferedWritter

---
+++ src/academy/devddojo/maratonajava/javacore/Vio/test/BufferedWritterTest01.java
@@ -0,0 +1,22 @@
+package academy.devddojo.maratonajava.javacore.Vio.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWritterTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true);
+             BufferedWriter br = new BufferedWriter(fw)) //Esse parenteses é porque a classe FileWriter implementa Closeable. Entao o try-withresources vai automaticamente fechar.
+        {
+            br.write("O DevDojo é  ");
+            br.newLine();
+            br.flush(); // Basicamente, o .flush() é um "empurrão" que garante que os dados saiam do buffer e cheguem ao destino final sem demora.
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- src/academy/devddojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
+++ src/academy/devddojo/maratonajava/javacore/Vio/test/FileReaderTest01.java
@@ -7,18 +7,19 @@ import java.io.IOException;
 public class FileReaderTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
-        try (FileReader fr = new FileReader(file)) {
- //           char [] in = new char[13];
-//            fr.read(in);
-//            for (char c : in) {
-//                System.out.print(c);
-//            }
-            int i;
-            while((i= fr.read())!= -1){
-                System.out.print((char)i);
+        try (FileReader fr = new FileReader(file)) {// Esse parenteses é porque a classe FileWriter implementa Closeable. Entao o try-withresources vai automaticamente fechar. {
+            char[] in = new char[1000];
+            int size = fr.read(in);
+            System.out.println(size);
+            for (char c : in) {
+                System.out.print(c);
             }
+//            int i;
+//            while((i= fr.read())!= -1){
+//                System.out.print((char)i);
+//            }
 
-        }  catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
